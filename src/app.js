7b9e15bf77f41c406302e492dda8d0c012a3621d
Sam Bernheim <sbernheim4@users.noreply.github.com>
2020-03-24 23:17:56 -0400

    fix: update email sending setup (#1)
    Co-authored-by: sbernheim1 <samuel.bernheim@disneystreaming.com>

--- src/app.js
+++ src/app.js
@@ -4,17 +4,16 @@ const hbs = require('hbs')
 const pg = require('pg')
 const bodyParser = require('body-parser')
 
-var db = require('../db/database');
+const db = require('../db/database');
 // var db = require('../email_sender');
-
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 const partialsPath = path.join(__dirname, '../partials')
 
 
-  app.use(bodyParser.json())
-  app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
@@ -24,6 +23,7 @@ hbs.registerPartials(partialsPath)
 //     if (err) throw err;
 //   });
 
+const sendEmail = require('./../email_sender');
 
 app.get('', (req, res) =>{
     res.render('index', {
@@ -37,20 +37,21 @@ app.get('/help', (req, res) =>{
 })
 
 app.get('/pairs', (req, res) =>{
-   
+
       db.query(
-        "select * from requests where start_time > 1585077926952 AND end_time < 1585080927527;", function(error, result, field) {
+          "select * from requests where start_time > 1585077926952 AND end_time < 1585080927527;",
+          function(error, result, field) {
             if(error) {
                 exist(error); //No error
             } else if(result) {
-                console.log(result); 
+                console.log(result);
             } else {
                 exist(null, null); //It is never execute
             }
         });
 })
 
-  
+
 
 app.post('/help', (req, res) =>{
     // db.connect(function(err) {
@@ -78,15 +79,23 @@ app.post('/help', (req, res) =>{
         }
       );
 
-      email_sender(email)
+    // Send an email with the below
+    const options = {
+        from: 'from@from.com',
+        to: 'to@to.com',
+        subject: "You're both free now."
+    };
+
+    sendEmail(options);
+
     // db.query('SELECT * FROM users').then(res => {
     //     const data = res.rows;
-      
+
     //     console.log('all data');
     //     data.forEach(row => {
     //         console.log(`Id: ${row.id} Name: ${row.name} Email: ${row.email}`);
     //     })
-      
+
     //   }).finally(() => {
     //     db.end()
     //   });
